5e6f9575ce43b83b2d60a8a0608c2680916a2cb9
Moksh <mokshdipikasikkem@msitprogram.net>
2020-07-25 06:18:04 +0530

    Jul 25  06:18:03

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -10,6 +10,32 @@
 # assert(nthHappyNumber(6) == 28)
 # assert(nthHappyNumber(7) == 31)
 
+def ishappy(n):
+	s=str(n)
+	if (n < 0):
+		return False
+	else:
+		while len(s) > 1:
+			t = 0
+			for i in t:
+				t += ((int(i))** 2)
+			s = str(t)
+		if int(s) == 1:
+			return True
+		return False
 
 def fun_nth_happy_number(n):
-	return 0
+	if n==0:
+		return 1
+	elif n == 1:
+		return 7
+	i = 2
+	count = 1
+	while (count <= n):
+		if ishappy(i) == True:
+			count += 1
+			if count == n:
+				return i
+		i += 1
+			
+	# return 0
