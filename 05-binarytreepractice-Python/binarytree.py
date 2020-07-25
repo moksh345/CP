23e26a318fcfd8410a9cbd81d981f88034a597c0
Moksh <mokshdipikasikkem@msitprogram.net>
2020-07-25 23:40:10 +0530

    Jul 25  23:40:10

--- 05-binarytreepractice-Python/binarytree.py
+++ 05-binarytreepractice-Python/binarytree.py
@@ -14,7 +14,7 @@ class BinaryTree(object):
         False otherwise."""
         # Your code goes here
         # pass
-        if find_val in self.root:
+        if find_val in self.root.value:
             return True
         return False
 
