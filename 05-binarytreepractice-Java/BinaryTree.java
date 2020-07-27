d84ee4e84b600a831c7c027644d8265e589f1d7c
Moksh <mokshdipikasikkem@msitprogram.net>
2020-07-27 13:16:58 +0530

    Jul 27  13:16:58

--- 05-binarytreepractice-Java/BinaryTree.java
+++ 05-binarytreepractice-Java/BinaryTree.java
@@ -13,17 +13,25 @@ public class BinaryTree {
 	public Node root;
 	
 	public BinaryTree(int value) {
-        // Your code goes here
+		// Your code goes here
+		this.root=new Node(value);
 	}
 
 	public boolean search(int value) {
 		// Your code goes here
-		return true;
+		Node node=this.root;
+		return search_Node(node,value);
 	}
 
 	private boolean search_Node(Node node, int value) {
 		// Your code goes here
+		if (node==null)
 		return false;
+		else if (node.value==value) return true;
+		boolean lft=search_Node(node.left,value);
+		if(lft) return lft;
+		boolean rht=search_Node(node.right,value);
+		return rht;
 
 	}
 }
