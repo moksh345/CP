70da66e1ef5f0cf1fb471336943991f5e6d70fb1
Moksh <mokshdipikasikkem@msitprogram.net>
2020-07-27 20:32:47 +0530

    Jul 27  20:32:46

--- 03-kth_occurrences-Python/kth_occurrences.py
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -5,6 +5,21 @@
 
 
 def fun_kth_occurrences(s, n):
+	s = s.strip()
+	li = list(s)
+	freq = {}
+	for item in li:
+		if item in freq:
+			freq[item] += 1
+		else:
+			freq[item] = 1
+			x = []
+	l = list(freq.values())
+	m = list(freq.keys())
+	l.sort(reverse=True)
+	p = list(freq.values())
+	for i in l:
+		t=m[p.index(i)]
 	return 'a'
 
 
