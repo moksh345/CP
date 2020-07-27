ce53d5c6c747851b17697a731ee06569c9bd0e75
Moksh <mokshdipikasikkem@msitprogram.net>
2020-07-27 21:39:36 +0530

    Jul 27  21:39:36

--- 09-shortenlongruns-Python/shortenlongruns.py
+++ 09-shortenlongruns-Python/shortenlongruns.py
@@ -7,8 +7,38 @@
 # assert(shortenLongRuns([2, 3, 5, 5, 5, 3], 3) == [2, 3, 5, 5, 3]) 
 # Note: your function may not just create a copy of L and call the destructive version of this function (below) on 
 # that copy and return it. Instead, you must directly construct the result here.
-
+def lookandsay(a):
+	# Your code goes here
+	b = a
+	b.append(" ")
+	result = []
+	repeat = b[0]
+	b = b[1:]
+	count = 1
+	if len(a) == 0:
+		return result
+	for i in b:
+		if i != repeat:
+			result.append((count, int(repeat)))
+			count = 1
+			repeat = i
+		else:
+			count += 1
+	return result
 
 def shortenlongruns(L, k):
 	# Your code goes here
+	t = lookandsay(L)
+	l = []
+	if len(L) == 0:
+		return l
+	for i in t:
+		if len(i) == 0:
+			return l
+		for i in range(i[0]):
+			if i == k - 1:
+				break
+			l.append(i[1])
+	return l
+
 	pass
